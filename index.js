693283cc72d3142c1ff7fabd3a9bcd1eb5d86778
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-03-11 14:37:46 -0500

    test

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
-
+'test'
 
 //(2) Bio of the third artist (2nd index) in the array 
 
